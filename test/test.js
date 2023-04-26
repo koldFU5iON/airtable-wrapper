c1bd4e935255cc9b8b562d3491b8b30175a1be22
Devon Stanton <devon.stanton@gmail.com>
2023-04-26 12:11:27 +0200

    updated tests to include name and id

--- test/test.js
+++ test/test.js
@@ -1,24 +1,29 @@
-import { expect } from 'chai';
-import * as dotenv from 'dotenv'; dotenv.config()
-import { airtableBase } from '../scripts/AirtableWrapper.js';
+import { expect } from "chai";
+import * as dotenv from "dotenv";
+dotenv.config();
+import { airtableBase } from "../scripts/AirtableWrapper.js";
 
-const baseID = 'tbl0XbaLEnIZAlABf' // need to change to a test base
+const baseID = "tbl0XbaLEnIZAlABf"; // need to change to a test base
 const base = airtableBase(baseID, process.env.AIRTABLE_API_KEY);
 
-describe('testing tests', () => {
-  it('should return true', () => {
-    expect(true).to.equal(true);
-  });
-});
-
 // test the API key works correctly and I recieve a response form the AirtableWrapper
-describe('AirtableWrapper Functionality', () => {
-
-  describe('Connect to Base', () => {
-    it('Should return a base object', () => {
-      expect(base).to.be.an('object');
+describe("AirtableWrapper Base Functionality", () => {
+  describe("Connect to Base", () => {
+    it("Should return a base object", () => {
+      expect(base).to.be.an("object");
     });
+
+    it("Should return a base object with a name property", () => {
+      expect(base).to.have.property("name");
+    });
+
+    it("Should return the id of the base called", () => {
+      expect(base.id).to.equal("tbl0XbaLEnIZAlABf");
+    });
+
+    it("Should return the name of the base called", () => {
+      expect(base.name).to.equal("Coverage Tracker 2.0");
+    });
+
   });
-
-
 });
