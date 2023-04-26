74c4938e1de67834cab02a97ddf9c8d0256624b2
Devon Stanton <devon.stanton@gmail.com>
2023-04-26 16:57:45 +0200

    updated tests to reflect new modules path

--- test/test.js
+++ test/test.js
@@ -1,14 +1,24 @@
 import { expect } from "chai";
 import * as dotenv from "dotenv";
 dotenv.config();
-import { airtableBase } from "../scripts/AirtableWrapper.js";
+import { airtableBase } from "../src/main.js";
 
-const baseID = "tbl0XbaLEnIZAlABf"; // need to change to a test base
+
+const baseID = "appX8bN5InXaMteca"; // need to change to a test base
 const base = airtableBase(baseID, process.env.AIRTABLE_API_KEY);
 
 // test the API key works correctly and I recieve a response form the AirtableWrapper
-describe("AirtableWrapper Base Functionality", () => {
+describe("Base Functionality", () => {
   describe("Connect to Base", () => {
+
+    it("Should check that the BaseID is valid", () => {
+      expect(() => airtableBase('baseID', process.env.AIRTABLE_API_KEY)).to.throw("Invalid base ID");
+    });
+
+    it("Should check that the API key is valid", () => {
+      expect(() => airtableBase(baseID,'Invalid ApiKey')).to.throw();
+    });
+
     it("Should return a base object", () => {
       expect(base).to.be.an("object");
     });
@@ -27,3 +37,24 @@ describe("AirtableWrapper Base Functionality", () => {
 
   });
 });
+
+describe("Table Functionality", () => {
+  describe("Get Table Object", () => {
+    it("Should return a table object", () => {
+      const table = base.getTable("Coverage");
+      expect(table).to.be.an("object");
+    });
+
+    it("Should return a table object with a name property", () => {
+      const table = base.getTable("Coverage");
+      expect(table).to.have.property("name");
+    });
+
+    it("Should return the id of the table called", () => {
+      const table = base.getTable("Coverage");
+      expect(table.id).to.equal("tbl0XbaLEnIZAlABf");
+    });
+  });
+});
+
+describe("Record Functionality", () => {});
