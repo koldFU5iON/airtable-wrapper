ee27d2fd90f2b42a5782c4f39ec811de452e6c4e
Devon Stanton <devon.stanton@gmail.com>
2023-04-26 10:50:07 +0200

    Initial tests, and installed chai

--- test/test.js
+++ test/test.js
@@ -1,11 +1,24 @@
-import assert from 'assert';
-import myModule from '../index';
-import dotenv from 'dotenv';
+import { expect } from 'chai';
+import * as dotenv from 'dotenv'; dotenv.config()
+import { airtableBase } from '../scripts/AirtableWrapper.js';
 
-dotenv.config({ path: '.env.test' });
+const baseID = 'tbl0XbaLEnIZAlABf' // need to change to a test base
+const base = airtableBase(baseID, process.env.AIRTABLE_API_KEY);
 
-describe('myModule', () => {
-  it('should return "Hello, World!" when called with no arguments', () => {
-    assert.equal(myModule.sayHello(), 'Hello, World!');
+describe('testing tests', () => {
+  it('should return true', () => {
+    expect(true).to.equal(true);
   });
+});
+
+// test the API key works correctly and I recieve a response form the AirtableWrapper
+describe('AirtableWrapper Functionality', () => {
+
+  describe('Connect to Base', () => {
+    it('Should return a base object', () => {
+      expect(base).to.be.an('object');
+    });
+  });
+
+
 });
