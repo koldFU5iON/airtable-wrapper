11df0aac76f7bb7c3b71376cf7c2f9b37ed5c595
Devon Stanton <devon.stanton@gmail.com>
2023-05-02 11:36:42 +0200

    added in JSDoc comments

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-import Airtable from "./src/Wrappers/AirtableAPI.js";
-
+/**
+ * Creates an instance of Airtable.
+ * @param {string} baseID - The ID of the base.
+ * @param {string} apiKey - The API key for authentication.
+ * @returns {Airtable} The created Airtable instance.
+ * @throws {Error} Throws an error if the base ID is invalid or if the API key is missing.
+ */
 export const airtable = (baseID, apiKey) => {
-  
   if (!baseID.startsWith("app") || null) {
     throw new Error("Invalid base ID");
   }
