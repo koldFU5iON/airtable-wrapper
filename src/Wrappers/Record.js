7fea4b04bfcbd0aa81d3aaf4e21e50dcc818c998
Devon Stanton <devon.stanton@gmail.com>
2023-05-03 13:29:12 +0200

    formatting and commenting record class

--- src/Wrappers/Record.js
+++ src/Wrappers/Record.js
@@ -1,22 +1,46 @@
-// Record is a wrapper around the Airtable Record object
+/**
+ * Represents a wrapper around the Airtable Record object.
+ */
 class Record {
+  /**
+   * Creates an instance of Record.
+   * @param {Airtable.Record} record - The Airtable Record object.
+   * @param {Array<string>} fields - The list of available fields.
+   */
   constructor(record, fields) {
     this.record = record;
     this.fields = fields;
   }
 
+  /**
+   * Retrieves the cell value for a given field.
+   * @param {string} field - The field name.
+   * @throws {Error} If the field is required, or if the field is not found.
+   * @returns {*} The cell value.
+   */
   getCellValue(field) {
     // error handling
     if (!field || field === null) throw new Error(`Field required`);
     if (!this.fields.some((f) => f === field)) {
       throw new Error(`Field, ${field} not found`);
     }
-        
+
     // return value
     return this.record.fields[field];
   }
 
+  /**
+   * Retrieves the cell value as a string for a given field.
+   * @param {string} field - The field name.
+   * @throws {Error} If the field is required, or if the field is not found.
+   * @returns {string} The cell value as a string, or an empty string if null or undefined.
+   */
   getCellValueAsString(field) {
+    // error handling
+    if (!field || field === null) throw new Error(`Field required`);
+    if (!this.fields.some((f) => f === field)) {
+      throw new Error(`Field, ${field} not found`);
+    }
     const value = this.getCellValue(field);
     return value !== null && value !== undefined ? String(value) : "";
   }
--- test/recordTests.js
+++ test/recordTests.js
@@ -12,6 +12,7 @@ describe("Record Functionality", function () {
   this.timeout(10000); // Set the timeout to 10000ms
 
   describe("Methods", () => {
+    // getCellValue()
     describe("getCellValue()", () => {
       it("Should return a method .getCellValue", async () => {
         const record = await table.selectRecordAsync(testRecord);
@@ -30,19 +31,52 @@ describe("Record Functionality", function () {
           "Field, Bad Field not found"
         );
       });
-      
 
       it("Should return an object for a single select field", async () => {
         const record = await table.selectRecordAsync(testRecord);
         const cellValue = record.getCellValue("Product");
         expect(cellValue).to.be.an("object");
       });
-      
 
       it("Should return a value from the field 'Product', Xerox 1980", async () => {
         const record = await table.selectRecordAsync(testRecord);
         expect(record.getCellValue("Product")).to.be.a("string");
         expect(record.getCellValue("Product")).to.equal("Xerox 1980");
+      });
+    });
+    // getCellValueAsString()
+    describe("getCellValueAsString()", () => {
+      it("Should return a method .getCellValueAsString", async () => {
+        const record = await table.selectRecordAsync(testRecord);
+        expect(record.getCellValueAsString).to.be.a("function");
+      });
+
+      it("Should throw an error if a field is not valid", async () => {
+        const record = await table.selectRecordAsync(testRecord);
+        expect(() => record.getCellValueAsString()).to.throw(
+          Error,
+          "Field required"
+        );
+      });
+
+      it("Should throw an error if a field is not found", async () => {
+        const record = await table.selectRecordAsync(testRecord);
+        await expect(() => record.getCellValueAsString("Bad Field")).to.throw(
+          Error,
+          "Field, Bad Field not found"
+        );
+      });
+
+      it("Should always return a string value", async () => {
+        const record = await table.selectRecordAsync(testRecord);
+        const cellValue = record.getCellValueAsString("Product");
+        expect(cellValue).to.be.a("string");
+      });
+
+      it("Should return a value from the field 'Product', Xerox 1980", async () => {
+        const record = await table.selectRecordAsync(testRecord);
+        expect(record.getCellValueAsString("Product")).to.be.a("string");
+        expect(record.getCellValueAsString("Product")).to.equal("Xerox 1980");
       });
     });
   });
